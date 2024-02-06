cc24e011dc8195bf7e1778c5e8016dd171370a61
Khalid Alnasser <loody170ss@gmail.com>
2024-02-06 20:02:31 +0300

    Updated tailwind.config.js
    removed "purge" option which was changed to "content"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/**/*.html'],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
