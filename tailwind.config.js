1683865dffa6343fa15c2ef8ea5576ebb2faa721
Khalid Alnasser <loody170ss@gmail.com>
2024-02-06 19:57:39 +0300

    Added more optimization to prepare for deployment
    Added PurgeCSS to purge any unused styles in my project to reduce size the size of my CSS file and improve load times

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/**/*.html'],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
